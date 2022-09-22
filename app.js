4d0767d2ae8d057cd282c7ecc2b507da3982054c
Miguel Ramos <return.infox@gmail.com>
2022-09-22 20:12:38 -0300

    configuracion del app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+require("dotenv").config(); //* para decirle a la aplicación que utilize las variables de entorno
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors()); //* para evitar el error de origen cruzado, por el momento todos los clientes se pueden conectar
+
+const port = process.env.PORT || 3001; //* en caso que llegue fallar tenemos el puerto 3001
+
+//* pasamos como primer argumento el puerto y luego una función 
+app.listen(port, () => {
+  //* alt + 96 
+  console.log(`http://localhost:${port}`);
+});
