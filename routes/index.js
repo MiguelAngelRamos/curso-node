e181080baf3631e2e95ba992a86f52583cb70a3c
Miguel Ramos <return.infox@gmail.com>
2022-09-26 19:49:03 -0300

    ruta dinamica creada

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,26 @@ const fs = require("fs"); //* para leer la ruta directorio
 const router = express.Router();
 
 const PATH_ROUTES = __dirname; //* Nos devuelve el path en donde esta ubicado este archivo "index.js" (ruta absoluta)
-console.log(PATH_ROUTES);
+//console.log(PATH_ROUTES);
+// const a = fs.readdirSync(PATH_ROUTES); //* retorna un array
+// [ 'index.js', 'users.js']
 
-const a = fs.readdirSync(PATH_ROUTES);
-console.log(a);
+const removeExtension = fileName => {
+  return fileName.split('.').shift(); //* solo el nombre
+};
+
+//* const removeExtension = fileName => fileName.split('.').shift(); //* solo el nombre
+
+fs.readdirSync(PATH_ROUTES).filter( file => {
+  console.log(file); //* index.js y users.js
+  const name = removeExtension(file);
+  if(name !== 'index') {
+    console.log('Loading...' + name);
+    //* alt + 96 ``
+    router.use(`/${name}`, (req, res) => {
+      res.send({ aviso: 'la ruta dinamica se creo'})
+    });
+  }
+});
+module.exports = router;
+//* http://localhost:3000/api/users
