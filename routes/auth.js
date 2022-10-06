cadeddd04257cd1f9f537f74643da4230b4317ba
Miguel Ramos <return.infox@gmail.com>
2022-10-06 19:47:02 -0300

    validadores de auth

---
+++ routes/auth.js
@@ -0,0 +1,12 @@
+const express = require("express");
+//* controladores loginController, registerController
+//* Modelo
+//* Validators para login y register
+const { validatorRegisterUser, validatorLoginUser } = require('../validators/auth-validators');
+const router = express.Router();
+
+//* http://localhost:3000/api/auth
+//* http://localhost:3000/api/auth/Login
+//* http://localhost:3000/api/auth/register
+
+router.post("/register", validatorRegisterUser, "registerController");
