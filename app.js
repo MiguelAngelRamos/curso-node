7e21e63a857e3c2b67eceed0fe72c29255d5af20
Miguel Ramos <return.infox@gmail.com>
2022-10-11 20:46:50 -0300

    configuracion del documentacion interfaz grafica ok

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 require("dotenv").config(); //* para decirle a la aplicación que utilize las variables de entorno
 const express = require("express");
 const cors = require("cors");
+//* Documentacion Swagger
+
+const swaggerUI = require("swagger-ui-express");
+const openApiConfiguration = require("./docs/swagger");
+
 const dbConnect = require("./config/mongo");
 const app = express();
 
@@ -8,6 +13,8 @@ app.use(cors()); //* para evitar el error de origen cruzado, por el momento todo
 app.use(express.json()); //* Con esto le indicamos a nuestro backend que reciba información en el método POST
 app.use(express.static("storage")); //* para hacer accesible las imagenes, le indicamos a express cual va ser la carpeta de los estaticos publicos
 const port = process.env.PORT || 3001; //* en caso que llegue fallar tenemos el puerto 3001
+
+app.use('/documentation', swaggerUI.serve, swaggerUI.setup(openApiConfiguration));
 
 //* Aqui vamos a cargar nuestras rutas
 //* http://localhost:3000/api/"nombreDeLaRuta"
